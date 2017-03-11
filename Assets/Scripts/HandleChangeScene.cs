707dac123a8aee43519b06b942d1955580e3c008
Tyler Johnson <tylermackj@gmail.com>
2017-03-10 23:27:35 -0600

    Main menu works, started game scene

---
+++ Assets/Scripts/HandleChangeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleChangeScene : MonoBehaviour {
+    public Transform mainmenuLocation;
+    public Transform gameLocation;
+    public Transform optionsLocation;
+    public Transform creditsLocation;
+    public void ChangeScene(int location)
+    {
+        switch (location)
+        {
+            case 0:
+                transform.position = mainmenuLocation.position;
+                break;
+            case 1:
+                transform.position = gameLocation.position;
+                break;
+            case 2:
+                transform.position = optionsLocation.position;
+                break;
+            case 3:
+                transform.position = creditsLocation.position;
+                break;
+            default:
+                break;
+        }
+    }
+}
