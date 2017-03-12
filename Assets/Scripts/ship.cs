1c90ccdcd25ed9468434fcc6061a81391ba9ddd7
Tyler Johnson <tylermackj@gmail.com>
2017-03-11 23:24:18 -0600

    Added options and credit scene

--- Assets/Scripts/ship.cs
+++ Assets/Scripts/ship.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ship : MonoBehaviour {
+public class Ship : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
